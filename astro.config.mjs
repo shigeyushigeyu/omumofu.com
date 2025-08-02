851400ef6660da48427e28622b14d9e24c18eb9a
SystemAdminCorporationon <user_system_admin@icloud.com>
2025-08-03 00:36:25 +0900

    変更内容を簡潔に書く

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,3 @@
-import { defineConfig } from 'astro/config';
-//import staticAdapter from '@astrojs/adapter-static';
-
 export default defineConfig({
-  output: 'static',
-  adapter: staticAdapter(),
+  // adapter: staticAdapter(),  // この行もコメントアウトまたは削除
 });
