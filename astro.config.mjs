19949c079d1af0f56a24f3109855320ea4431dc8
SystemAdminCorporationon <user_system_admin@icloud.com>
2025-08-05 00:58:51 +0900

    変更内容を簡潔に書く

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'server',        // SSRを有効に
-  adapter: cloudflare(),   // Cloudflare向けに最適化
-  // mode: "nodejs" は書かない
+  // outputの指定はしない（デフォルトの 'static' になる）
+  adapter: cloudflare({
+    // SSRを有効に　有効にするとmessageboard.astroが動かない
+    // SSRされるページ（prerender=false）のために、
+    // Node.js互換モードは有効にしておく
+    mode: "nodejs_compat"
+  })
 });
