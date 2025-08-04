c13580215d5e69adc56b39dfb45bd1591a3c405b
SystemAdminCorporationon <user_system_admin@icloud.com>
2025-08-05 00:46:57 +0900

    変更内容を簡潔に書く

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  //output: 'server',        // SSRを有効に
+  output: 'server',        // SSRを有効に
   adapter: cloudflare(),   // Cloudflare向けに最適化
   // mode: "nodejs" は書かない
 });
